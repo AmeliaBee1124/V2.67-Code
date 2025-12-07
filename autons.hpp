b1eb4c378d0c8932ffa7e32b1208ef71391df857
AmeliaBee1124 <ameliabee1124@gmail.com>
2025-12-07 10:01:20 -0500

    Update autons.hpp

--- autons.hpp
+++ autons.hpp
@@ -2,8 +2,10 @@
 
 void default_constants();
 
-void red_right();
-void red_left();
+void red_right_S();
+void red_right_C();
+void red_left_S();
+void red_left_C();
 void blue_right();
 void blue_left();
 void Main_Skills();
